f1e80996e468a52e9fbbd8e631efb6621f887342
Marlon <m.c4n1as@gmail.com>
2018-02-13 00:52:24 -0700

    Part 5
    The javascript file has new functions. checkRow, getBox,checkForWinner. These functions allow the game to prevent a player move to from being overridden at a square and when three in a row happens, a message is displayed that says who the winner is.

--- HTML5Application/public_html/Scripts/ticTacToeScript.js
+++ HTML5Application/public_html/Scripts/ticTacToeScript.js
@@ -13,16 +13,54 @@ function setMessage(msg){
 }
 
 function nextMove(square){
-    square.innerText = document.turn;
-    switchTurn();
+    if(square.innerText ===""){
+        square.innerText = document.turn;
+        switchTurn();
+    }
+    else{
+        setMessage("That square is already used.");
+    }
 }
 
 function switchTurn(){
-    if(document.turn =="X"){
+    if(checkForWinner(document.turn)){
+        setMessage("Congratulations, " +document.turn+"! You win!");
+    }
+    else if(document.turn ==="X"){
         document.turn = "O";
+        setMessage("It's "+document.turn + " turn!");
     }
     else{
         document.turn = "X";
+        setMessage("It's "+document.turn + " turn!");
     }
-    setMessage("It's "+document.turn + " turn!");
+    
+}
+
+function checkForWinner(move){
+    var result = false;
+    if(checkRow(1,2,3,move)|| //checks rows
+       checkRow(4,5,6,move)||
+       checkRow(7,8,9,move)||
+       checkRow(1,4,7,move)|| //checks columns
+       checkRow(2,5,8,move)||
+       checkRow(3,6,9,move)||
+       checkRow(1,5,9,move)||
+       checkRow(3,5,7,move)){
+       result = true;
+    }
+    return result;
+}
+
+//move is either X or O
+function checkRow(a,b,c,move){
+    var result = false;
+    if(getBox(a)== move && getBox(b)== move && getBox(c)== move){
+        result= true;
+    }
+    return result;
+}
+
+function getBox(number){
+    return document.getElementById("s"+number).innerText;
 }
